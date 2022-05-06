6f5ca61021adc68004dc8d0dd3412d29357b3680
Noid3ah <micropeddie@outlook.com>
2022-05-06 14:11:56 -0230

    Added two more functions checkWinner and playAgain
    checkWinner will compare both scores at the end and alert 'win' 'lose' or 'draw'
    playAgain will ask the user for another game with a confirm prompt. if yes; pass the game() function else alert bye.

--- main.js
+++ main.js
@@ -7,13 +7,15 @@
 
 const choices = ["rock", "paper", "scissors"];
 let score = 0;
+let playerScore = 0;
+let computerScore = 0;
 let round = 0;
 
 function playerChoice() {
   let input = prompt("Choose rock, paper or scissors:");
   for (let i = 0; i < 5; i++) {
     if (input === "rock" || input === "paper" || input === "scissors") {
-      console.log(`Player: ${input}`);
+      // console.log(`Player: ${input}`);
       break;
     } else if (input == null) {
       break;
@@ -27,26 +29,72 @@ function playerChoice() {
 
 function computerChoice() {
   let compInput = choices[Math.floor(Math.random() * choices.length)];
-  console.log(`Computer: ${compInput}`);
+  // console.log(`Computer: ${compInput}`);
   return compInput;
 }
 
 function compareInputs(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
-    console.log("It's a tie!");
+    console.log(
+      ` Round${round} It's a tie!. Score: ${playerScore}:${computerScore}`
+    );
   } else if (playerChoice === "rock" && computerChoice === "scissors") {
-    console.log(`You won! ${playerChoice} beats ${computerChoice}`);
+    playerScore++;
+    console.log(
+      `Round${round} You won! ${playerChoice} beats ${computerChoice}. Score: ${playerScore}:${computerScore}`
+    );
   } else if (playerChoice === "paper" && computerChoice === "rock") {
-    console.log(`You won! ${playerChoice} beats ${computerChoice}`);
+    playerScore++;
+    console.log(
+      `Round${round} You won! ${playerChoice} beats ${computerChoice}. Score: ${playerScore}:${computerScore}`
+    );
   } else if (playerChoice === "scissors" && computerChoice === "paper") {
-    console.log(`You won! ${playerChoice} beats ${computerChoice}`);
+    playerScore++;
+    console.log(
+      `Round${round} You won! ${playerChoice} beats ${computerChoice}. Score: ${playerScore}:${computerScore}`
+    );
   } else {
-    console.log(`You lost! ${computerChoice} beats ${playerChoice}`);
+    computerScore++;
+    console.log(
+      `Round${round} You lost! ${computerChoice} beats ${playerChoice}. ${playerScore}:${computerScore}`
+    );
+  }
+}
+
+// compare wins function: if playerscore > computerscore player wins else computer wins
+// if firstround; round = 1
+// if player beats computer;  score = 1:0
+
+function checkWinner() {
+  if (playerScore === computerScore) {
+    alert("It was a Draw");
+  } else if (playerScore > computerScore) {
+    alert("YOU WON!");
+  } else {
+    alert("YOU LOST!");
   }
 }
 
 function game() {
-  compareInputs(playerChoice(), computerChoice());
+  for (let i = 0; i < 5; i++) {
+    round++;
+    console.log(`Round ${round}`);
+    compareInputs(playerChoice(), computerChoice());
+  }
+  checkWinner(compareInputs());
+  playAgain();
+}
+
+// if game ends prompt play again?
+// if ok? play again else break
+
+function playAgain() {
+  let play = confirm("Play again?");
+  if (play) {
+    game();
+  } else {
+    alert("Bye");
+  }
 }
 
 game();
